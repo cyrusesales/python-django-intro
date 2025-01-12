189ac98c15c1d949e1e02afea5a4e93fe7400895
Cyrus Sales <cyrus.sales@outlook.com>
2025-01-12 18:48:06 +0800

    displaying posts with template tags

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post #import the Post
 # Create your views here.
 
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date') # get all of the object for us, order by descending way minus sign
+    return render(request, 'posts/posts_list.html', {'posts': posts}) #add posts in dictionary, passing data on the template
