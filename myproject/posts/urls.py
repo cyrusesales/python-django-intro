fe51f1b614dc3d35271b1757385a2100bf148861
Cyrus Sales <cyrus.sales@outlook.com>
2025-01-14 00:06:35 +0800

    creating a post page template

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views # import view.py
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.posts_list), # homepage function in views.py
+    path('', views.posts_list, name="list"), # homepage function in views.py
+    path('<slug:slug>', views.post_page, name="page"), # use slug path converted, left and parameter
 ]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Post #import the Post
+
 # Create your views here.
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date') # get all of the object for us, order by descending way minus sign
     return render(request, 'posts/posts_list.html', {'posts': posts}) #add posts in dictionary, passing data on the template
+
+# create function for post page
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug) # pass to get one post we have that matches slug
+    return render(request, 'posts/post_page.html', {'post': post}) #add posts in dictionary, passing data on the template
