396a5a3a09dde8fc49b0b576d3420831d4baf944
Cyrus Sales <cyrus.sales@outlook.com>
2025-01-18 17:32:09 +0800

    adding the form to a template

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # after successfully submitted it will  redirect to post list
+from django.contrib.auth.forms import UserCreationForm # importing a form
 
 # Create your views here.
 def register_view(request):
-    return render(request, "users/register.html")
+    # if the form has been submitted, this function will handle this request
+    if request.method == "POST":
+        form = UserCreationForm(request.POST) #form validation
+        if form.is_valid(): # will validate if true
+                form.save()
+                return redirect("posts:list")
+    else:
+         form = UserCreationForm()
+    form = UserCreationForm()
+    return render(request, "users/register.html", { "form": form })
