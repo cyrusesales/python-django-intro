925077111cfe9915e9848194219473e36f47f306
Cyrus Sales <cyrus.sales@outlook.com>
2025-01-22 23:53:20 +0800

    create custom form

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User # add author of the post
 # Create your models here.
 
 class Post(models.Model):
@@ -10,6 +10,10 @@ class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     # means not requiring an image if we dont have one
     banner = models.ImageField(default='fallback.png', blank=True)
+    # author is the foreign key into 2 tables
+    # on_delete thats telling the database how to handle data if this relationship is deleted
+    # CASCADE meanse if deleted. it delete all post too
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.title
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post #import the Post
 from django.contrib.auth.decorators import login_required # decorator on function,
-
+from . import forms
 # Create your views here.
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date') # get all of the object for us, order by descending way minus sign
     return render(request, 'posts/posts_list.html', {'posts': posts}) #add posts in dictionary, passing data on the template
 
-# create function for post page
-def post_page(request, slug):
+def post_page(request, slug): # create function for post page
     post = Post.objects.get(slug=slug) # pass to get one post we have that matches slug
     return render(request, 'posts/post_page.html', {'post': post}) #add posts in dictionary, passing data on the template
 
 @login_required(login_url="/users/login") #checks when function runs to see if user login
 def post_new(request):
-    return render(request, 'posts/post_new.html')
+    if request.method == 'POST': # should be POST request
+        form = forms.CreatePost(request.POST, request.FILES) #sending also an image file
+        if form.is_valid(): # validate what submitted against the model
+            # save with user
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect('posts:list')
+    else: # else is the GET request
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', { 'form': form })
